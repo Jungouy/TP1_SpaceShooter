738e57742f8111c9cc0be526cf4a0a9819b62948
RAFAEL CAFÉ <rcafe@etu.uqac.ca>
2025-09-20 19:18:19 -0400

    avancement 20-09

---
+++ TP1_SpaceShooter/Source/TP1_SpaceShooter/Private/Exhortation.cpp
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Public/Exhortation.h"
+
+#include "Components/BoxComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "GameFramework/PawnMovementComponent.h"
+
+
+// Sets default values
+AExhortation::AExhortation()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>("BoxCollision");
+	RootComponent = BoxCollision;
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetupAttachment(BoxCollision);
+
+	MovementComponent = CreateDefaultSubobject<UPawnMovementComponent, UFloatingPawnMovement>(TEXT("MovementComponent0"));
+	MovementComponent->UpdatedComponent = BoxCollision;
+}
+
+// Called when the game starts or when spawned
+void AExhortation::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AExhortation::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	FVector Pos = GetActorLocation();
+	Pos.Z = FMath::Clamp(Pos.Z, ZMin, ZMax);
+	Pos.Y = FMath::Clamp(Pos.Y, YMin, YMax);
+	SetActorLocation(Pos);
+}
+
+// Called to bind functionality to input
+void AExhortation::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	check(PlayerInputComponent)
+	PlayerInputComponent->BindAxis(TEXT("MoveUpDownZ"), this, &AExhortation::MoveUpDown);
+	PlayerInputComponent->BindAxis(TEXT("MoveRightLeftY"), this, &AExhortation::MoveRightLeft);
+	PlayerInputComponent->BindAction(TEXT("Fire!"), IE_Pressed, this, &AExhortation::Fire);
+}
+
+void AExhortation::MoveUpDown(float Value)
+{
+	if (MovementComponent  && FMath::Abs(Value) > KINDA_SMALL_NUMBER)
+	{
+		AddMovementInput(GetActorUpVector(), Value);
+	}
+}
+
+void AExhortation::MoveRightLeft(float Value)
+{
+	if (MovementComponent && FMath::Abs(Value) > KINDA_SMALL_NUMBER)
+	{
+		AddMovementInput(GetActorRightVector(), Value);
+	}
+}
+void AExhortation::Fire()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, "coming soon");
+}
+
---
+++ TP1_SpaceShooter/Source/TP1_SpaceShooter/Public/Exhortation.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Exhortation.generated.h"
+
+UCLASS()
+class TP1_SPACESHOOTER_API AExhortation : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AExhortation();
+	UPROPERTY(VisibleAnywhere)
+	class UBoxComponent* BoxCollision;
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* StaticMesh;
+
+	UPROPERTY(Category = Pawn, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UPawnMovementComponent* MovementComponent;
+
+	void MoveUpDown(float Value);
+	void MoveRightLeft(float Value);
+	void Fire();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	float YMax = 1890.0f;
+	float YMin = -1890.0f;
+	float ZMax = 1060.0f;
+	float ZMin = -1060.0f;
+};
